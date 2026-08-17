1ab89aac08b2e493056c9c2998f58fffd191b61e
arsh720 <arsh45803@gmail.com>
2026-08-17 22:58:36 +0530

    auth.model leetcode question

--- src/auth/model.py
+++ src/auth/model.py
@@ -1,39 +1,29 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        import math
-        #length at a point is 4*m+n
-        m=len(matrix)
-        n=len(matrix[0])
-        right =m*n -1
-        left=0
-        mid=(left+right)//2
-        if target>matrix[m-1][n-1] or target<matrix[0][0]:
-            return False
-        
-        def bin(matrix,target,left,right):
-            mid=(left+right)//2
-            row=mid//n
-            col=mid%n
-            if matrix[row][col]== target:
-                return True
-            if matrix[row][col] > target:
-                right=mid-1
-                return bin(matrix,target,left,right)
-            if matrix[row][col]<target:
-                left=mid+1
-                return bin(matrix,target,left,right)
-            return False
-        bin(matrix,target,left,right)
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
 
 
-        
-        
-        
-        
+class User(SQLModel, table=True):
+    __tablename__ = "user_accounts"
 
-        
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid.uuid4,
+            info={"description": "Unique identifier for the user account"},
+        )
+    )
+
+    username: str
+    first_name: str = Field(nullable=True)
+    last_name: str = Field(nullable=True)
+    is_verified: bool = False
+    email: str
+    password_hash: str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=func.now))
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
